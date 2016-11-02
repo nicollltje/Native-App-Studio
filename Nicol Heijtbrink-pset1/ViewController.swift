30068c2e41acdd410e2c754f8b88ccae36cfd79e
Nicol Heijtbrink <nicolheijtbrink@hotmail.com>
2016-11-02 22:34:55 +0100

    All images added to the storyboard and view controller and one global switch function created though not implemented yet

--- Nicol Heijtbrink-pset1/ViewController.swift
+++ Nicol Heijtbrink-pset1/ViewController.swift
@@ -9,11 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // links all the images to outlets to the viewcontroller
+    @IBOutlet weak var arms: UIImageView!
+    @IBOutlet weak var nose: UIImageView!
+    @IBOutlet weak var shoes: UIImageView!
+    @IBOutlet weak var mustache: UIImageView!
+    @IBOutlet weak var mouth: UIImageView!
+    @IBOutlet weak var hat: UIImageView!
+    @IBOutlet weak var glasses: UIImageView!
+    @IBOutlet weak var eyes: UIImageView!
+    @IBOutlet weak var eyebrows: UIImageView!
+    @IBOutlet weak var ears: UIImageView!
+
+    @IBAction func toggleSwitch(_ sender: UISwitch) {
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+         }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
